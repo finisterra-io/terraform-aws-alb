5d70fb546a2c6be58126d54b310f4864b5c35f3f
xneyder <xneyder@gmail.com>
2023-09-09 23:15:42 -0400

    Add files

--- main.tf
+++ main.tf
@@ -234,7 +234,7 @@ resource "aws_lb_listener" "this" {
     type             = each.value.listener_fixed_response != null ? "fixed-response" : "forward"
 
     dynamic "fixed_response" {
-      for_each = each.value.listener_fixed_response != null ? [each.value.listener_fixed_response] : []
+      for_each = each.value.listener_fixed_response != {} ? [each.value.listener_fixed_response] : []
       content {
         content_type = fixed_response.value["content_type"]
         message_body = fixed_response.value["message_body"]
